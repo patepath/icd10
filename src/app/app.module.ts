c9ce5ca38774cb814c5fd46e7fbd8b1f78eb8fa4
patepath <patepath@gmail.com>
2023-03-31 20:07:58 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {InputTextModule} from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
